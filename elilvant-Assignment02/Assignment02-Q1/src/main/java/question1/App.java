cc1768ad393783ee96d186b20d0ee5522589bcd0
tharnyelilvannan <tharny2005@outlook.com>
2024-09-17 13:03:56 -0400

    Working on Question 1

--- elilvant-Assignment02/Assignment02-Q1/src/main/java/question1/App.java
+++ elilvant-Assignment02/Assignment02-Q1/src/main/java/question1/App.java
@@ -1,10 +1,23 @@
 package question1;
 
 /**
- * Hello world!
+ * Author: Tharny Elilvannan
+ * Last Updated: September 17, 2024
+ * Purpose: Converts a decimal to an octal number. 
  */
+
+import java.util.Scanner;
+
 public class App {
+
+    private static class Converter extends Object {
+
+    } // end of Converter class
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+        Scanner userInput = new Scanner(System.in);
+
+	System.out.println("Enter decimal number: ");
+	int decimal = userInput.nextInt(); 
+    } // end of main method
+} // end of App class
