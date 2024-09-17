fba7aaf738c0e90ea8179ce173ccea362d5e2577
tharnyelilvannan <tharny2005@outlook.com>
2024-09-17 13:14:55 -0400

    Add Question 2 from before

--- elilvant-Assignment02/Assignment02-Q2/src/main/java/question2/App.java
+++ elilvant-Assignment02/Assignment02-Q2/src/main/java/question2/App.java
@@ -1,7 +1,9 @@
 package question2;
 
 /**
- * Hello world!
+ * Author: Tharny Elilvannan
+ * Last Updated: September 17, 2024
+ * Purpose: Prints integer numbers divisible by 3, 5, or both
  */
 public class App {
     public static void main(String[] args) {
