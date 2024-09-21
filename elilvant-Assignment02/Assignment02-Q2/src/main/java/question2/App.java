5198ad01028b392b86ea6d55c86038c0e57f7bc8
tharnyelilvannan <tharny2005@outlook.com>
2024-09-21 16:04:39 -0400

    Fixed date

--- elilvant-Assignment02/Assignment02-Q2/src/main/java/question2/App.java
+++ elilvant-Assignment02/Assignment02-Q2/src/main/java/question2/App.java
@@ -2,7 +2,7 @@ package question2;
 
 /**
  * Author: Tharny Elilvannan
- * Last Updated: September 17, 2024
+ * Last Updated: September 21, 2024
  * Purpose: Prints integer numbers divisible by 3, 5, or both
  */
 
