e792eab532c0e71d9137634ae54702d6a525893f
tharnyelilvannan <tharny2005@outlook.com>
2024-09-21 16:28:39 -0400

    Finish Question 3

--- elilvant-Assignment02/Assignment02-Q3/src/main/java/question3/App.java
+++ elilvant-Assignment02/Assignment02-Q3/src/main/java/question3/App.java
@@ -1,10 +1,45 @@
 package question3;
 
 /**
- * Hello world!
+ * Author: Tharny Elilvannan
+ * Last Updated: September 21, 2024
+ * Purpose: Prints numbers in a triangle
  */
+
+import java.util.Scanner;
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+        Scanner userInput = new Scanner(System.in);
+
+	System.out.print("Enter integer: ");
+	
+	int number = userInput.nextInt();
+
+	// calls method to print out triangle
+	Triangle t = new Triangle();
+	t.Triangle(number);
+
+    } // end of main method
+
+} // end of App class
+
+class Triangle {
+
+    // prints numbers in a triangle
+    public static void Triangle(int number) {
+	int i = 1;
+	String triangle = "";
+	
+	// determines next number in triangle
+	while (i <= number) {
+	    triangle = triangle + Integer.toString(i);
+	    System.out.println(triangle);
+	    i = i + 1;
+
+	} // end of while
+
+    } // end of Triangle method
+
+} // end of Triangle class
