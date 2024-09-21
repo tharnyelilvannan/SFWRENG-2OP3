9952e94c86d233290a9a8a4e2af0fad65c9885e6
tharnyelilvannan <tharny2005@outlook.com>
2024-09-21 13:09:29 -0400

    Started Question 2

--- elilvant-Assignment02/Assignment02-Q2/src/main/java/question2/App.java
+++ elilvant-Assignment02/Assignment02-Q2/src/main/java/question2/App.java
@@ -13,9 +13,36 @@ public class App extends Object {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
-	System.out.println("Enter integer: ");
+	System.out.print("Enter integer: ");
 	int number = userInput.nextInt();
 	
 	    } // end of main method
 
 } // end of App class
+
+class Divide {
+    public static void Divide(int number) {
+	
+    }
+
+    private static String divideByThree(int number) {
+	int i = 1;
+	String byThree = "";
+
+	System.out.print("Divided by 3 -> ");
+	while (i <= number) {
+	    if ((i % 3) == 0) {
+		byThree = byThree + Integer.toString(i) + ","
+	    } // end of if
+	    
+	    i = i +1;
+	} // end of while
+	
+	lengthByThree = byThree.length();
+	byThree = byThree.substring(0, lengthByThree - 1);
+
+	System.out.println(byThree);
+	
+    } // end of divideByThree method
+
+} // end of Divide class
