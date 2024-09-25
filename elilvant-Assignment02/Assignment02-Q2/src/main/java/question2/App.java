42b10dba6a5d2c0c5b2225781c17e2d254ca4f2f
tharnyelilvannan <tharny2005@outlook.com>
2024-09-25 10:55:58 -0400

    Added comments and fixed Question 2

--- elilvant-Assignment02/Assignment02-Q2/src/main/java/question2/App.java
+++ elilvant-Assignment02/Assignment02-Q2/src/main/java/question2/App.java
@@ -2,7 +2,7 @@ package question2;
 
 /**
  * Author: Tharny Elilvannan
- * Last Updated: September 21, 2024
+ * Last Updated: September 25, 2024
  * Purpose: Prints integer numbers divisible by 3, 5, or both
  */
 
@@ -13,118 +13,138 @@ public class App extends Object {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
-	// user enters an integer 	
-	System.out.print("Enter integer: ");
-	int number = userInput.nextInt();
+		// user enters an integer 	
+		System.out.print("Enter integer: ");
+		int number = userInput.nextInt();
 	
-	// integers divisible by 3 and 5 are printed
-	Divide d = new Divide();
-	d.Divide(number);
-	
-	    } // end of main method
+		// integers divisible by 3 and 5 are printed
+		Divide d = new Divide(number);
+
+	} // end of main method
 
 } // end of App class
 
 class Divide {
 
-    public static void Divide(int number) {
+    Divide(int number) {
 
-	divideByThree(number);
-	divideByFive(number);
-	divideByBoth(number);
+		divideByThree(number);
+		divideByFive(number);
+		divideByBoth(number);
 
     } // end of Divide method
 
     // prints numbers divisible by 3
     private static void divideByThree(int number) {
-	int counter = 1;
-	String byThree = "";
+		int counter = 1;
+		String byThree = "";
 
-	System.out.print("Divided by 3 ->");
+		System.out.print("Divided by 3 ->");
 
-	// if number is less than 3, there are no numbers divisible by 3
-	if (number < 3) {
-	    System.out.println(" None.");
-        }
-	else {
-	    while (counter <= number) {
-	        if ((counter % 3) == 0) {
-		    byThree = byThree + " " + Integer.toString(counter) + ",";
-	        } // end of if statements
+		// if number is less than 3, there are no numbers divisible by 3
+		if (number < 3) {
+
+		    System.out.println(" None.");
+
+   	    }
+		else {
+
+		    while (counter <= number) {
+
+	    	    if ((counter % 3) == 0) {
+
+			   		byThree = byThree + " " + Integer.toString(counter) + ",";
+
+	    	    } // end of if statement
 	    
-	        counter = counter +1;
-	    } // end of while
+	        	counter = counter +1;
 
-		// formats output 
-		int lengthByThree = byThree.length();
-		byThree = byThree.substring(0, lengthByThree - 1);
-		byThree = byThree + ".";
+	    	} // end of while
 
-		System.out.println(byThree);
+			// formats output 
+			int lengthByThree = byThree.length();
+			byThree = byThree.substring(0, lengthByThree - 1);
+			byThree = byThree + ".";
 
-	} // end of if/else statement
+			System.out.println(byThree);
+
+		} // end of if/else statement
 		
-    } // end of divideByThree method
+	} // end of divideByThree method
 
     // divides by 5
     private static void divideByFive(int number) {
-	int counter = 1;
-	String byFive = "";
+		int counter = 1;
+		String byFive = "";
 
-	System.out.print("Divided by 5 ->");
+		System.out.print("Divided by 5 ->");
 	
-	// if number is less than 5, there are no numbers divisible by 5
-	if (number < 5) {
-	    System.out.println(" None.");
-	}
-	else {
-	    while (counter <= number) {
-	        if ((counter % 5) == 0) {
-		    byFive = byFive + " " + Integer.toString(counter) + ",";
-	        } // end of if statement
+		// if number is less than 5, there are no numbers divisible by 5
+		if (number < 5) {
+
+		    System.out.println(" None.");
+
+		}
+		else {
+
+	    	while (counter <= number) {
+
+	        	if ((counter % 5) == 0) {
+
+		    	byFive = byFive + " " + Integer.toString(counter) + ",";
+
+	        	} // end of if statement
 	    
-	        counter = counter +1;
-	    } // end of while
+	        	counter = counter +1;
 
-		// formats output
-		int lengthByFive = byFive.length();
-		byFive = byFive.substring(0, lengthByFive - 1);
-		byFive = byFive + ".";
+	    	} // end of while
 
-		System.out.println(byFive);
+			// formats output
+			int lengthByFive = byFive.length();
+			byFive = byFive.substring(0, lengthByFive - 1);
+			byFive = byFive + ".";
 
-	} // end of if/else statement
+			System.out.println(byFive);
+
+		} // end of if/else statement
 	
     } // end of divideByFive method
 
     // prints numbers divisible by both 3 and 5
     private static void divideByBoth(int number) {
-	int counter = 1;
-	String byBoth = "";
+		int counter = 1;
+		String byBoth = "";
 	
-	System.out.print("Divided by 3 and 5 ->");
+		System.out.print("Divided by 3 and 5 ->");
 
-	// if the number is less than 5, then there are no numbers divisible by both
-	if (number <= 5) {
-	    System.out.println(" None.");
-	}
-	else {
-	    while (counter <= number) {
-	        if ((counter % 5 == 0) & (counter % 3 == 0)) {
-	            byBoth = byBoth + " " + Integer.toString(counter) + ",";
-	        } // end of if statement
+		// if the number is less than 5, then there are no numbers divisible by both
+		if (number <= 5) {
+
+	    	System.out.println(" None.");
+
+		}
+		else {
+
+	    	while (counter <= number) {
+
+	        	if ((counter % 5 == 0) & (counter % 3 == 0)) {
+
+	            	byBoth = byBoth + " " + Integer.toString(counter) + ",";
+
+	        	} // end of if statement
 	    
-	        counter = counter + 1;
-	    } // end of while
-	
-	    // formats output
-	    int lengthByBoth = byBoth.length();
-	    byBoth = byBoth.substring(0, lengthByBoth - 1);
-	    byBoth = byBoth + ".";
-	
-	    System.out.println(byBoth);
+	        	counter = counter + 1;
 
-	} // end of if/else statement
+	    	} // end of while
+	
+	    	// formats output
+	    	int lengthByBoth = byBoth.length();
+	    	byBoth = byBoth.substring(0, lengthByBoth - 1);
+	    	byBoth = byBoth + ".";
+	
+	    	System.out.println(byBoth);
+
+		} // end of if/else statement
 
     } // end of divideByBoth method
 
