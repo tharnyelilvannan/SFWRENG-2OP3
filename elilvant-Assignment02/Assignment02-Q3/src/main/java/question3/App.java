40fd732ca681007c49b7de9b8f8bf2dd0856808c
tharnyelilvannan <tharny2005@outlook.com>
2024-09-25 11:02:08 -0400

    Added comments and fixed Question 3

--- elilvant-Assignment02/Assignment02-Q3/src/main/java/question3/App.java
+++ elilvant-Assignment02/Assignment02-Q3/src/main/java/question3/App.java
@@ -2,43 +2,50 @@ package question3;
 
 /**
  * Author: Tharny Elilvannan
- * Last Updated: September 21, 2024
+ * Last Updated: September 25, 2024
  * Purpose: Prints numbers in a triangle
  */
 
 import java.util.Scanner;
 
+
 public class App {
 
     public static void main(String[] args) {
+
         Scanner userInput = new Scanner(System.in);
 
-	System.out.print("Enter integer: ");
+		System.out.print("Enter integer: ");
 	
-	int number = userInput.nextInt();
+		int number = userInput.nextInt();
 
-	// calls method to print out triangle
-	Triangle t = new Triangle();
-	t.Triangle(number);
+		// calls method to print out triangle
+		Triangle t = new Triangle(number);
+
+		userInput.close();
 
     } // end of main method
 
 } // end of App class
 
+
 class Triangle {
 
     // prints numbers in a triangle
-    public static void Triangle(int number) {
-	int counter = 1;
-	String triangle = "";
-	
-	// determines next number in triangle
-	while (counter <= number) {
-	    triangle = triangle + Integer.toString(counter);
-	    System.out.println(triangle);
-	    counter = counter + 1;
+    Triangle(int number) {
 
-	} // end of while
+		int counter = 1;
+		String triangle = "";
+	
+		// determines and prints next line in triangle
+		while (counter <= number) {
+
+	    	triangle = triangle + Integer.toString(counter);
+	    	System.out.println(triangle);
+
+	    	counter = counter + 1;
+
+		} // end of while
 
     } // end of Triangle method
 
