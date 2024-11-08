cfffb129bff131a77a3024d78db1ea4df5a9cd52
tharnyelilvannan <tharny2005@outlook.com>
2024-11-07 22:26:00 -0500

    Q2

--- elilvant-Assignment04/Assignment04-Q2/src/main/java/question2/App.java
+++ elilvant-Assignment04/Assignment04-Q2/src/main/java/question2/App.java
@@ -1,10 +1,72 @@
 package question2;
 
 /**
- * Hello world!
+ * Author: Tharny Elilvannan
+ * Last Updated: November 08, 2024
+ * Purpose: Calculates the average and median of a list of integers.
  */
+
+import java.util.*;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+	public static void main(String[] args) {
+        	int[] hello = {4, 3, 2, 9, 8};
+
+			StatisticsCalculator s = new StatisticsCalculator(hello);
+			System.out.println(s.calculateAverage());
+			System.out.println(s.calculateMedian());
+	} // end of main method
+} // end of App class
+
+class StatisticsCalculator {
+	double average = 0.0;
+	double median = 0.0;
+	double sum = 0;
+	int[] intArray;
+
+	StatisticsCalculator(int[] listOfInt) {
+		intArray = sortArray(listOfInt);
+		calculateSum();
+	} // end of StatisticsCalculator method
+
+	private void calculateSum() {
+		int i;
+
+		for (i =0; i < intArray.length; i++) {
+			sum = sum + intArray[i];
+		} // end of for loop
+	} // end of calculateSum method
+
+	public double calculateAverage() {
+		average = sum / (intArray.length);
+		return average;
+	} // end of calculateAverage method
+
+	private int[] sortArray(int[] listOfInt) {
+		Arrays.sort(listOfInt);
+		return listOfInt;
+	} // end of sortArray method
+ 
+	public double calculateMedian() {
+
+		if ((intArray.length % 2) == 0) {
+			median = (intArray[(intArray.length)/2] + intArray[((intArray.length)/2)+1])/2;
+		}
+		else {
+			median = intArray[(intArray.length)/2];
+		}
+
+		return median;
+
+	} // end of calculateMedian method
+
+	public void updateArray(int[] newArray) {
+		intArray = sortArray(newArray);
+		calculateSum();
+	} // end of updateArray method
+
+	public int[] getArray() {
+		return intArray;
+	}
+
+} // end of StatisticsCalculator class
