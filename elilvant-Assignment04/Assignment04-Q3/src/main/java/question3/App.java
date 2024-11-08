aee1eff4232ff670b7aa24d4967c594bc8c76b5f
tharnyelilvannan <tharny2005@outlook.com>
2024-11-07 23:17:07 -0500

    Q3

--- elilvant-Assignment04/Assignment04-Q3/src/main/java/question3/App.java
+++ elilvant-Assignment04/Assignment04-Q3/src/main/java/question3/App.java
@@ -1,10 +1,48 @@
 package question3;
 
 /**
- * Hello world!
+ * Author: Tharny Elilvannan
+ * Last Updated: November 07, 2024
+ * Purpose: Processed strings.
  */
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String myString = "hello!   my name is Tharny?";
+        StringCleaner c = new StringCleaner(myString);
+        System.out.println(c.getString());
     }
 }
+
+class StringCleaner {
+
+    String strArray;
+    
+    StringCleaner(String str) {
+        strArray = removePunctuation(str);
+        strArray = convertToLowercase(strArray);
+        strArray = removeSpaces(strArray);
+    } // end of StringCleaner method
+
+    private String removePunctuation(String str) {
+        str = str.replaceAll("[':.,|///;?!@#$%^&*()~]", "");
+        return str;
+    } // end of remove method
+
+    private String convertToLowercase(String str) {
+        str = str.toLowerCase();
+        return str;
+    } // end of convert method
+
+    private String removeSpaces(String str) {
+        str = str.replaceAll("\s\s+", "\s");
+        str = str.replaceAll("\s{1,}$", "");
+        str = str.replaceAll("^\s{1,}", "");
+
+        return str;
+    } // end of removeSpaces method
+
+    public String getString() {
+        return strArray;
+    } // end of getString method
+} // end of StringCleaner class
